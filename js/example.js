af332e709a1c76306d7620084cf4f83d2f6d9136
crazzov118 <joycrazzov@gmail.com>
2023-09-14 14:39:20 -0700

    Fixed MUC

--- js/example.js
+++ js/example.js
@@ -61,7 +61,7 @@ function subscribeToInstantLogin() {
    .then(function() {
       //console.log('>>> CONNECTION READY')
       $('body').addClass('jsxc-fullscreen jsxc-two-columns')
-         jsxc.manualJoin(localStorage.getItem("userid") + "@xmpp.meetstream.com", "ROOM");
+         jsxc.manualJoin(localStorage.getItem("userid") + "@xmpp.meetstream.com", "room10", localStorage.getItem("userid"));
    }).catch(function(err) {
       console.log('>>> catch', err)
    })
