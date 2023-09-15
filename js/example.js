4baba516eb8f8e9f50f4253d39f27883340d26c0
crazzov118 <joycrazzov@gmail.com>
2023-09-15 03:55:01 -0700

    changed the creating roomname mutable

--- js/example.js
+++ js/example.js
@@ -61,7 +61,8 @@ function subscribeToInstantLogin() {
    .then(function() {
       //console.log('>>> CONNECTION READY')
       $('body').addClass('jsxc-fullscreen jsxc-two-columns')
-         jsxc.manualJoin(localStorage.getItem("userid") + "@xmpp.meetstream.com", "room10", localStorage.getItem("userid"));
+      var roomName = "room";
+      jsxc.manualJoin(localStorage.getItem("userid") + "@xmpp.meetstream.com", roomName, localStorage.getItem("userid"));
    }).catch(function(err) {
       console.log('>>> catch', err)
    })
